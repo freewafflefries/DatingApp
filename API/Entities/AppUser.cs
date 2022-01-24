acc72afefc212ab90cd7a9325389534f535a0761
Scott W <scottaza@gmail.com>
2022-01-23 18:13:55 -0700

    initial commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//We are going to use EntityFramwork to help maintain DB tables that always match to our Entities classes
+//So when I modify the definition of the calss, the DB table for it iwll update as well (code-first migrations)
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
